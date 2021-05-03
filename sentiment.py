3e110d63f21c2b2032efff0b75f1aec28d6f7e98
Lathiya Dhruvraj <32243179+lathiyadhruvraj@users.noreply.github.com>
2021-05-03 21:52:02 +0530

    Update sentiment.py

--- sentiment.py
+++ sentiment.py
@@ -11,15 +11,15 @@ warnings.filterwarnings('ignore')
 
 # credentials
 load_dotenv()
-# consumer_key = os.getenv('consumer_key')
-# consumer_secret = os.getenv('consumer_secret')
-# access_token = os.getenv('access_token')
-# access_token_secret = os.getenv('access_token_secret')
+consumer_key = os.getenv('consumer_key')
+consumer_secret = os.getenv('consumer_secret')
+access_token = os.getenv('access_token')
+access_token_secret = os.getenv('access_token_secret')
 
-consumer_key = os.environ['consumer_key']
-consumer_secret = os.environ['consumer_secret']
-access_token = os.environ['access_token']
-access_token_secret = os.environ['access_token_secret']
+# consumer_key = os.environ['consumer_key']
+# consumer_secret = os.environ['consumer_secret']
+# access_token = os.environ['access_token']
+# access_token_secret = os.environ['access_token_secret']
 
 
 
